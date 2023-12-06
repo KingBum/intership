1f0b548a03ce8a9c0d4388fbf783555288f4331f
Dat51034 <dat51034@donga.edu.vn>
2023-12-06 16:31:06 +0700

    add logging

--- src/main/java/com/example/springdemo/SpringDemoApplication.java
+++ src/main/java/com/example/springdemo/SpringDemoApplication.java
@@ -1,15 +1,24 @@
 package com.example.springdemo;
 
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.slf4j.Logger;
 
 
 @SpringBootApplication
 public class SpringDemoApplication {
+	private static final Logger logger = LoggerFactory.getLogger(SpringDemoApplication.class);
 
 	public static void main(String[] args) {
+		logger.trace("A TRACE Message");
+		logger.debug("A DEBUG Message");
+		logger.info("An INFO Message");
+		logger.warn("A WARN Message");
+		logger.error("An ERROR Message");
 		SpringApplication.run(SpringDemoApplication.class, args);
 	}
-	
+
 }
